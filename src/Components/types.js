813eccbca7c90b33004bd4b0e56b80275cba5438
Abhishek Jha <69615011+Abhishekjha52@users.noreply.github.com>
2023-01-19 11:21:26 +0530

    Update types.js

--- src/Components/types.js
+++ src/Components/types.js
@@ -2,7 +2,6 @@
 
 export const ADD_USER = 'ADD_USER';
 export const EDIT_USER = 'EDIT_USER';
-export const FETCH_USERS = 'FETCH_USERS';
 export const DELETE_USER = 'DELETE_USER';
 export const RESET_USER = 'RESET_USER';
 export const UPDATE_FORM_DATA = 'UPDATE_FORM_DATA';
